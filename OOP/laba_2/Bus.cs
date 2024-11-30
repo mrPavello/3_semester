85f10d2d5a2324ac816fcc836ecd3fa954d00396
Pavel Redzko <153453532+mrPavello@users.noreply.github.com>
2024-12-01 01:20:19 +0300

    Add files via upload

---
+++ OOP/laba_2/Bus.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Reflection.Metadata.Ecma335;
+
+namespace laba_2
+{
+    internal partial class Bus
+    {
+        string fio;
+        string num_of_bus;
+        string rote_number;
+        string bus_brand;
+        int start_year;
+        int mileage;
+
+        static int bus_count;
+        static string id;
+        public const int max_mileage = 10000000;
+
+        static Bus()
+        {
+            bus_count = 0;
+            id = Guid.NewGuid().ToString();
+        }
+
+        public Bus() : this("неизвестный", "не определён", "не определён", "не определён", 2000, 1000) { }
+        
+        public Bus(
+            string fio = "неизвестный",
+            string num_of_bus = "не определён",
+            string rote_number = "не определён",
+            string bus_brand = "не определён",
+            int start_year = 2000,
+            int mileage = 1000
+            )
+            {
+                if (string.IsNullOrEmpty(fio) || 
+                    string.IsNullOrEmpty(num_of_bus) || 
+                    string.IsNullOrEmpty(rote_number) || 
+                    string.IsNullOrEmpty(bus_brand) || 
+                    start_year < 1980 || 
+                    start_year > DateTime.Now.Year || 
+                    mileage < 0)
+                {
+                    throw new ArgumentException("Некорректные данные");
+                }
+            
+            bus_count++;
+            this.fio = fio;
+            this.num_of_bus = num_of_bus;
+            this.rote_number = rote_number;
+            this.bus_brand = bus_brand;
+            this.start_year = start_year;
+            this.mileage = mileage;
+        }
+
+        public Bus(
+        string fio = "неизвестный",
+        string num_of_bus = "не определён",
+        string rote_number = "не определён")
+        {
+            if (string.IsNullOrEmpty(fio) ||
+                string.IsNullOrEmpty(num_of_bus) ||
+                string.IsNullOrEmpty(rote_number)
+                )
+            {
+                throw new ArgumentException("Некорректные данные");
+            }
+
+            bus_count++;
+            this.fio = fio;
+            this.num_of_bus = num_of_bus;
+            this.rote_number = rote_number;
+            this.bus_brand = "не определён";
+            this.start_year = DateTime.Now.Year;
+            this.mileage = 0;
+        }
+
+        public string Fio
+        {
+            get => fio;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("Строка не может быть пустой");
+                }
+                else if (!Regex.IsMatch(value, @"^[а-яА-Яa-zA-Z\s\.\-]+$"))
+                {
+                    Console.WriteLine("В строке присутсвует недопустимый символ.");
+                }
+                else if (value.Length < 4 || value.Length > 50)
+                {
+                    Console.WriteLine("Недопустимая длина строки.");
+                }
+                else
+                {
+                    fio = value;
+                }
+
+            }
+        }
+
+        public string NumOfBus
+        {
+            get => num_of_bus;
+            private set
+            {
+                if (!Regex.IsMatch(value, @"^[ABEIKMHOPCTX1-7\-]+$"))
+                {
+                    Console.WriteLine("Недопустимый символ в номере автобуса.");
+                }
+                else if (string.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("Строка не может быть пустой.");
+                }
+                else
+                {
+                    num_of_bus = value;
+                }
+            }
+        }
+
+        public string RoteNumber
+        {
+            get => rote_number;
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[0-9ABCЭ]+$"))
+                {
+                    Console.WriteLine("Недопустимый символ в номере маршрута.");
+                } 
+                else if (string.IsNullOrWhiteSpace (value))
+                {
+                    Console.WriteLine("Строка не может быть пустой.");
+                }
+                else
+                {
+                    rote_number = value;
+                }
+            }
+        }
+
+        public string BusBrand
+        {
+            get => bus_brand;
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[a-zA-Zа-яА-Я\.]+$"))
+                {
+                    Console.WriteLine("Недопустимый символ в строке.");
+                }
+                else if (string.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("Строка не может быть пустой.");
+                }
+                else
+                {
+                    bus_brand = value;
+                }
+            }
+        }
+
+        public int StartYear
+        {
+            get => start_year;
+            set
+            {
+                if (value < 1980 || value > DateTime.Now.Year)
+                {
+                    Console.WriteLine("Невозможное значение года.");
+                }
+                else
+                {
+                    start_year = value;
+                }
+            }
+        }
+
+        public int Mileage
+        {
+            get => mileage;
+            set
+            {
+                if (value < 0 || value > int.MaxValue)
+                {
+                    Console.WriteLine("Недопустимое значчение.");
+                }
+                else
+                {
+                    mileage = value;
+                }
+            }
+        }
+
+        public string ID => id;
+
+
+    }
+
+    internal partial class Bus
+    {
+        public override string ToString()
+        {
+            return $"Type: Bus\n" +
+                $"ФИО: {fio}\n" +
+                $"Номер автобуса: {num_of_bus}\n" +
+                $"Номер маршрута: {rote_number}\n" +
+                $"Марка: {bus_brand}\n" +
+                $"Начало эксплуатации: {start_year}г.\n" +
+                $"Пробег: {mileage}км.";
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(fio, num_of_bus, rote_number, bus_brand, start_year, mileage);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Bus thisBus)
+            {
+                return fio == thisBus.fio &&
+                    num_of_bus == thisBus.num_of_bus &&
+                    rote_number == thisBus.rote_number &&
+                    bus_brand == thisBus.bus_brand &&
+                    start_year == thisBus.start_year &&
+                    mileage == thisBus.mileage;
+            }
+            return false;
+        }
+
+        public int GetBusAge()
+        {
+            return DateTime.Now.Year - start_year;
+        }
+
+        public static void ClassBusInfo()
+        {
+            Console.WriteLine($"Количество экземпляров класса Bus: {bus_count}");
+        }
+
+        public static void UpdateBusMileage(ref Bus bus, int new_mileage, out string message)
+        {
+            if (new_mileage >= 0)
+            {
+                bus.Mileage = new_mileage;
+                message = "Пробег успешно обновлен.";
+            }
+            else
+            {
+                message = "Недопустимое значение";
+            }
+        }
+    }
+}
---
+++ OOP/laba_2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using laba_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bus rider0 = new Bus();
+        Bus rider1 = new Bus("Василий В.В.", "AD 2342-4", "33c", "mersedes");
+        Bus rider2 = new Bus("Павел В.В.", "AD 5321-4", "23", "mersedes", 2012, 12223);
+        Bus rider3 = new Bus("Владислав В.В.", "AD 4342-4", "12", "mersedes", 2012, 12411);
+        Bus rider4 = new Bus("Илья В.В.", "AD 7446-4", "33c", "mersedes", 2012, 23922);
+
+        Console.WriteLine(rider0.Fio);
+        Console.WriteLine(rider1.NumOfBus);
+        Console.WriteLine(rider2.RoteNumber);
+        Console.WriteLine(rider3.BusBrand);
+        Console.WriteLine(rider4.StartYear);
+        Console.WriteLine(rider3.Mileage);
+        Console.WriteLine(rider4.ID);
+        Console.WriteLine();
+        Console.WriteLine(rider0.ToString());
+        Console.WriteLine(rider0.GetHashCode());
+        Console.WriteLine(rider1.Equals(rider3));
+        Console.WriteLine(rider3.GetType());
+        Console.WriteLine(rider3.GetBusAge());
+        Bus.ClassBusInfo();
+
+        
+        Bus[] buses = {rider0, rider1, rider2, rider3, rider4};
+
+        string BusRoteNumber = "33c";
+        Console.WriteLine($"\nАвтобусы с номером маршрута {BusRoteNumber}:\n");
+        foreach (Bus bus in buses.Where(b => b.RoteNumber == BusRoteNumber))
+        {
+            Console.WriteLine($"{bus.ToString()}\n");
+
+        }
+
+        int serviceLife = 15;
+        Console.WriteLine($"Автобусы, которые эксплуатируются больше {serviceLife}г.\n");
+        foreach(Bus bus in buses.Where(b => b.GetBusAge() > serviceLife))
+        {
+            Console.WriteLine($"{bus.ToString()}\n");
+        }
+
+        string message;
+        Bus.UpdateBusMileage(ref rider3, 10, out message);
+        Console.WriteLine(message);
+
+        Console.WriteLine(rider3.ToString());
+
+        var myType = new
+        {
+            fio = "Овчинников А. А.",
+            num_of_bus = "AB2442-1",
+            rote_number = "22c",
+            bus_brand = "Volvo",
+            start_year = 2015,
+            mileage = 30000
+        };
+        Console.WriteLine(myType);
+    }
+}
